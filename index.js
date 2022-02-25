5c0e02ffd22b9b4b78d5b1cb6aea467eda3c4773
Miguel Gonzalez <miguel.gonzalez6@vodafone.com>
2022-02-25 15:27:27 +0000

    Add some code

---
+++ index.js
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Helo frmo Github'),
+    };
+    return response;
+};
+
